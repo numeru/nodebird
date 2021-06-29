5e60f18c1abde1757e1ba4d27e23ca55e5be4f71
numeru <cyk000418@naver.com>
2021-06-29 13:05:03 +0900

    add project setting

---
+++ front/pages/signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppLayout from "../components/AppLayout";
+
+const Signup = () => {
+  return (
+    <AppLayout>
+      <>signup</>
+    </AppLayout>
+  );
+};
+
+export default Signup;
